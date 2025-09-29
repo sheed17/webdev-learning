cf6ae01f196b718b824a681e17d821f4499e6d3d
sheed17 <rsamady2@gmail.com>
2025-09-28 22:55:04 -0700

    Update rendering-html.js

--- backend/express/rendering-html.js
+++ backend/express/rendering-html.js
@@ -1,1 +1,28 @@
+/* The way it works
+Server grabs data (from DB, API, etc.).
 
+Server passes that data into an EJS template.
+
+Template looks like HTML but has <%= %> and <% %> tags.
+
+These tags let you “manipulate and mess around” with data: loops, conditions, etc.
+
+EJS engine generates the final HTML.
+
+Client (browser) just displays that HTML.
+
+*/
+
+
+const express = require("express");
+const app = express();
+
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  res.render("index", {text: "World"})
+  
+})
+
+app.listen(3000);
